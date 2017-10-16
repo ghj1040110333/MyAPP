2f0e788a96289a412217ece502285d54914ec117
ghj1040110333 <1040110333@qq.com>
2017-10-16 10:48:16 +0800

    Initial commit

---
+++ app/src/androidTest/java/helloworld/top/ghaijun/www/myapp/ExampleInstrumentedTest.java
@@ -0,0 +1,29 @@
+package helloworld.top.ghaijun.www.myapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import com.alibaba.fastjson.JSON;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+
+        assertEquals("helloworld.top.ghaijun.www.myapp", appContext.getPackageName());
+    }
+}
